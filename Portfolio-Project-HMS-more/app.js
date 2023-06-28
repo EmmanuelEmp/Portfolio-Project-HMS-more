ba5eea2c211f6a0af9e78f280ed65eac3a530a90
EmmanuelEmp <emmyochogwu@gmail.com>
2023-06-29 00:56:54 +0100

    My Hospital management system project

---
+++ Portfolio-Project-HMS-more/app.js
@@ -0,0 +1,33 @@
+var i = 0;
+var images = [];
+var time = 3000;
+
+//Image list
+images[0]='doc1.jpg';
+images[1]='doc2.jpg';
+images[2]='doc3.jpg';
+images[3]='doc4.jpg';
+images[4]='doc5.jpg';
+
+//Change image
+function changeImg(){
+    document.slide.src = images[i];
+    if (i < images.length - 1){
+        i++;
+    }else{
+        i = 0;
+    }
+    setTimeout("changeImg()", time)
+}
+
+window.onload = changeImg;
+
+// Javascript to activate the harmburger menu
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-items');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
